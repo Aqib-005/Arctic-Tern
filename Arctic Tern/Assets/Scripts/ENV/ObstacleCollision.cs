7d55023a1e5e6c1b9de88992c838ec577fe439ff
Aqib-005 <shihanaqib@gmail.com>
2024-01-21 15:23:00 +0000

    added sfx for falling

--- Arctic Tern/Assets/Scripts/ENV/ObstacleCollision.cs
+++ Arctic Tern/Assets/Scripts/ENV/ObstacleCollision.cs
@@ -6,11 +6,14 @@ public class ObstacleCollision : MonoBehaviour
 {
     public GameObject theBird;
     public GameObject charModel;
+    public AudioSource crashThud;
+    public GameObject mainCam;
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<MeshCollider>().enabled = false;    
         theBird.GetComponent<BirdMove>().enabled = false;
         charModel.GetComponent<Animator>().Play("Falling Back Death");
-       // charModel.GetComponent<Animator>().SetTrigger("FallingBackDeathTrigger");
+       crashThud.Play();
+       mainCam.GetComponent<Animator>().enabled = true;
     }
 }
