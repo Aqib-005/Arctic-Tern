bec7aea93aed89f75f09a7467e93b3b553466523
Aqib-005 <shihanaqib@gmail.com>
2024-01-22 15:04:44 +0000

    added main menu

--- Arctic Tern/Assets/Scripts/ENV/EndRun.cs
+++ Arctic Tern/Assets/Scripts/ENV/EndRun.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndRun : MonoBehaviour
 {
@@ -22,5 +23,7 @@ public class EndRun : MonoBehaviour
         endScreen.SetActive(true);
         yield return new WaitForSeconds(3);
         endFadeOut.SetActive(true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(0);
     }
 }
